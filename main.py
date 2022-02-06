299741f249eae07be2a8b6520d8262ec9bc6e51c
lilu <23082010qQ>
2022-02-06 12:18:40 +0300

    Structuring the script

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ from pandas_main import fetch_wines_catalog
 import datetime
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-
 WINERY_YEAR_BIRTH = 1920
 WINERY_AGE = str(datetime.date.today().year - WINERY_YEAR_BIRTH)
-winery_catalog = fetch_wines_catalog()
-template = env.get_template('template.html')
 
 
-rendered_page = template.render(
-    winery_age=WINERY_AGE,
-    winery_catalog=winery_catalog,
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    winery_catalog = fetch_wines_catalog()
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        winery_age=WINERY_AGE,
+        winery_catalog=winery_catalog,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
